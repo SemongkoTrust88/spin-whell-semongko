93e9c541c42d222b43854467d38d62f771805e35
SemongkoTrust88 <belgiaslg1@gmail.com>
2026-08-09 14:37:32 +0700

    Delete public-config.mjs

--- public-config.mjs
+++
@@ -1,7 +0,0 @@
-import { loadConfig, json } from "./_lib.mjs";
-export default async () => {
-  const config = await loadConfig();
-  const safe = structuredClone(config);
-  delete safe.tickets;
-  return json(safe, 200, { "cache-control": "no-store" });
-};
